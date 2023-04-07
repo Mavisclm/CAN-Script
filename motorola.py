169a2c31e17d8f0dc97a0cd0cfd461bef7870374
Mavisclm <1541097082@qq.com>
2023-04-07 14:01:13 +0800

    Update motorola.py
    详情请看代码

--- motorola.py
+++ motorola.py
@@ -1,15 +1,16 @@
 import math
 
-# def bytes_hex(data):
-#     hex_data = []
-#     for i in range(len(data)):
-#         datas = data[i]
-#         # print(type(datas))
-#         hex_data.append(datas)
-#     return hex_data
-
-def motorola(value,startbit:int,width:int):
-
+def motorola(startbit:int,width:int,phy_value,offset,resolution):
+    """
+    :param startbit: 起始位
+    :param width: 位宽
+    :param phy_value: 物理值
+    :param offset: 偏移量
+    :param resolution: 精度
+    :return:
+    """
+    #原始值=（物理值-偏移量）/精度
+    value = int((phy_value-offset)/resolution)
     data = [0,0,0,0,0,0,0,0]
     #求起始字节
     #//：求商的整数
@@ -25,36 +26,29 @@ def motorola(value,startbit:int,width:int):
     elif flag < 0:
         # math.ceil：向上取整
         lines = math.ceil((width + startoffsetbit) / 8)
-        # print(lines)
+    elif flag == 0:
+        lines = 1
     #10进制转2进制
     binvalue = bin(value)[2:]
     # print(binvalue)
     if lines == 1:
-        # 右填充0,8代表补充至8位
-        finalvalue = binvalue.rjust(8,'0')
-        # print(finalvalue)
+        # 从右填充0,8代表补充至8位
+        finalvalue = binvalue.rjust(width,'0')  #在左边填充0至位宽
+        finalvalue = finalvalue.ljust(startoffsetbit+1,'0')     #右边空位补充0
         #2进制转十进制
         decimal = int(finalvalue, 2)
-        # print(decimal)
-        #10进制转16进制
-        hexadecimal = hex(decimal).upper()    #type:str
-        # print(decimal)
         #写入数据
-        data[startbyte] = hexadecimal
+        data[startbyte] = decimal
 
     #跨字节
     elif lines > 1:
         binvalue = binvalue.rjust(width, '0')
-        # print(binvalue)
         #第一个字节填充数据
         finalvalue1 = binvalue[:startoffsetbit+1]
-        # print(finalvalue1)
         # 2进制转十进制
         decimal1 = int(finalvalue1, 2)
-        # 10进制转16进制
-        hexadecimal1 = hex(decimal1).upper()    #type:str
         # 写入数据
-        data[startbyte] = hexadecimal1
+        data[startbyte] = decimal1
 
         #填充后面的字节
         remainbits = width - (startoffsetbit + 1)
@@ -62,64 +56,40 @@ def motorola(value,startbit:int,width:int):
 
         if remainbytes == 1:
             finalvalue2 = binvalue[startoffsetbit + 1:]
-            # 右填充0,8代表补充至8位
-            finalvalue2 = finalvalue2.rjust(8, '0')
-            # print(finalvalue2)
+            # 从右填充0,8代表补充至8位
+            finalvalue2 = finalvalue2.ljust(8, '0')
             # 2进制转十进制
             decimal2 = int(finalvalue2, 2)
-            # 10进制转16进制
-            hexadecimal2 = hex(decimal2).upper()    #type:str
             # 写入数据
-            data[startbyte + 1] = hexadecimal2
+            data[startbyte + 1] = decimal2
         i = 1
         while remainbytes > 1:
             finalvalue3 = binvalue[startoffsetbit + 1 + 8 * (i - 1):startoffsetbit + 1 + 8 * i]
-            # finalvalue3 = binvalue[startoffsetbit + 2:startoffsetbit + 2 + 8]
-            # print(finalvalue3+'12X')
             if finalvalue3 == '':
                 data[startbyte + i] = 0
             elif finalvalue3 != '':
-                # print('finalvalue3='+finalvalue3)
                 # 2进制转十进制
                 decimal3 = int(finalvalue3, 2)
-                # 10进制转16进制
-                hexadecimal3 = hex(decimal3).upper()    #type:str
                 # 写入数据
-                data[startbyte + i] = hexadecimal3
+                data[startbyte + i] = decimal3
 
             i += 1
-            # print("i="+str(i))
             remainbits = width - (startoffsetbit + 1) - 8 * (i - 1)
-            # print(remainbits)
             remainbytes = math.ceil(remainbits / 8)
-            # print('remainbytes='+str(remainbytes))
             if remainbytes == 1:
                 finalvalue4 = binvalue[startoffsetbit + 1 + 8 * (i - 1):]
-                # 右填充0,8代表补充至8位
+                # 从右填充0,8代表补充至8位
                 finalvalue4 = finalvalue4.rjust(8, '0')
-                # print(finalvalue4)
                 # 2进制转十进制
                 decimal4 = int(finalvalue4, 2)
-                # 10进制转16进制
-                hexadecimal4 = hex(decimal4).upper()    #type:str
                 # 写入数据
-                data[startbyte + i] = hexadecimal4
-                # break
-            # else:
-            #     i += 1
-            #     continue
-    # data = data.replace("'","")
-    # print(data)
+                data[startbyte + i] = decimal4
+
     return data
-    # return bytes_hex(data)
 
 if __name__ == '__main__':
-    motorola(15360, 39, 14)
-    print(motorola(15360, 39, 14))
+    print(motorola(39,14,18,3,0.0009765625))
+    print(motorola(31, 8, 55,0,1))
+    print(motorola(54, 1, 1, 0, 1))
+    print(motorola(45, 3, 6, 0, 1))
     # print(list(map(lambda x, y: x + y, motorola(12, 19, 4), motorola(15360, 39, 14))))
-    print(motorola(12, 19, 4))
-    # print(motorola(12, 19, 4)+motorola(15360, 39, 14))
-    # motorola(2, 21, 2)
-    # motorola(1, 7, 2)
-    motorola(1234567, 15, 32)
-    print(motorola(1234567, 15, 32))
